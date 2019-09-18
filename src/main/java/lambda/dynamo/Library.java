9c7d63be99a3995df24ee51c54b6b2c099a6355e
Nhu Trinh <nqtrinh87@gmail.com>
2019-09-18 12:33:33 -0700

    all methods work

--- src/main/java/lambda/dynamo/Library.java
+++ src/main/java/lambda/dynamo/Library.java
@@ -3,8 +3,52 @@
  */
 package lambda.dynamo;
 
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.datamodeling.*;
+import lambda.dynamo.Models.History;
+import lambda.dynamo.Models.Task;
+
 public class Library {
-    public boolean someLibraryMethod() {
-        return true;
-    }
+  private DynamoDB dynamoDb;
+  private String DYNAMODB_TABLE_NAME = "taskmaster";
+  private Regions REGION = Regions.US_WEST_2;
+
+  public Task save(Task task){
+    History h = new History("available");
+    task.addHistory(h);
+    final AmazonDynamoDB ddb = AmazonDynamoDBClientBuilder.defaultClient();
+    DynamoDBMapper ddbMapper = new DynamoDBMapper(ddb);
+    ddbMapper.save(task);
+    return task;
+  }
+
+  public Task updateAssignee(Task task){
+    final AmazonDynamoDB ddb = AmazonDynamoDBClientBuilder.defaultClient();
+    DynamoDBMapper ddbMapper = new DynamoDBMapper(ddb);
+    Task t = ddbMapper.load(Task.class, task.getId());
+    t.setAssignee(task.getAssignee());
+    t.setStatus("assigned");
+    t.addHistory(new History("assigned"));
+    ddbMapper.save(t);
+    return t;
+  }
+
+  public Task updateState(Task task){
+    final AmazonDynamoDB ddb = AmazonDynamoDBClientBuilder.defaultClient();
+    DynamoDBMapper ddbMapper = new DynamoDBMapper(ddb);
+    Task t = ddbMapper.load(Task.class, task.getId());
+      if(t.getStatus().equals("available")){
+        t.setStatus("assigned");
+      } else if(t.getStatus().equals("assigned")){
+        t.setStatus("accepted");
+      } else if(t.getStatus().equals("accepted")){
+        t.setStatus("finished");
+      }
+      t.addHistory(new History(t.getStatus()));
+      ddbMapper.save(t);
+      return t;
+  }
 }
